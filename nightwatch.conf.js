09185c4633e9aa6f92099c5bcca8ca07789fe7c9
Merge Desenv <luiz.miranda@portalunimed.com.br>
2019-01-04 12:19:59 -0200

    Adicionado massa de testes

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,8 +2,6 @@ require('env2')('.env'); // optionally store your environment variables in .env
 const seleniumServer = require("selenium-server");
 const chromedriver = require("chromedriver");
 const geckodriver = require("geckodriver");
-const PKG = require('./package.json'); // so we can get the version of the project
-const SCREENSHOT_PATH = "./node_modules/nightwatch/screenshots/" + PKG.version + "/";
 
 const config = { // we use a nightwatch.conf.js file so we can include comments and helper functions
   "src_folders": [
@@ -21,7 +19,6 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
       "webdriver.gecko.driver" : geckodriver.path,
     }
   },
-  "test_workers" : {"enabled" : true, "workers" : "auto"}, // perform tests in parallel where possible
   "test_settings": {
     "default": {
       "launch_url": "http://localhost",
@@ -55,9 +52,13 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
     "desenv": {
       "launch_url": "${ENV_URL_DESENV}",
       "selenium_port": 80,
-      "silent": true,
       "globals": {
         "waitForConditionTimeout": 10000    // wait for content on the page before continuing
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true
       }
     },
     "test": {
@@ -66,6 +67,11 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
       "silent": true,
       "globals": {
         "waitForConditionTimeout": 10000    // wait for content on the page before continuing
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true
       }
     },
     "pre": {
@@ -74,6 +80,11 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
       "silent": true,
       "globals": {
         "waitForConditionTimeout": 10000    // wait for content on the page before continuing
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true
       }
     },
     "prd-1": {
@@ -82,6 +93,11 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
       "silent": true,
       "globals": {
         "waitForConditionTimeout": 10000    // wait for content on the page before continuing
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true
       }
     },
     "chrome": { // your local Chrome browser (chromedriver)
@@ -142,28 +158,3 @@ const config = { // we use a nightwatch.conf.js file so we can include comments 
   }
 }
 module.exports = config;
-
-function padLeft (count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
-  return count < 10 ? '0' + count : count.toString();
-}
-
-var FILECOUNT = 0; // "global" screenshot file count
-/**
- * The default is to save screenshots to the root of your project even though
- * there is a screenshots path in the config object above! ... so we need a
- * function that returns the correct path for storing our screenshots.
- * While we're at it, we are adding some meta-data to the filename, specifically
- * the Platform/Browser where the test was run and the test (file) name.
- */
-function imgpath (browser) {
-  var a = browser.options.desiredCapabilities;
-  var meta = [a.platform];
-  meta.push(a.browserName ? a.browserName : 'any');
-  meta.push(a.version ? a.version : 'any');
-  meta.push(a.name); // this is the test filename so always exists.
-  var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
-  return SCREENSHOT_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
-}
-
-module.exports.imgpath = imgpath;
-module.exports.SCREENSHOT_PATH = SCREENSHOT_PATH;
--- test/e2e/botaoPesquisar.js
+++ test/e2e/botaoPesquisar.js
@@ -2,18 +2,62 @@ var config = require('../../nightwatch.conf.js');
 var gm = "/guia-medico";
 
 module.exports = {
-    'Testar botão pesquisar Busca Rápida e Texto': function (browser) {
-        let url = browser.launchUrl + gm
+
+    'Verificar se o botão pesquisar esta ativo': function (browser) {
+        let url = browser.launchUrl + gm;
         browser
             .url(url)
             .waitForElementVisible('body')
-            .pause(2000)
-            .clearValue('input[id=campo_pesquisa]')
-            .setValue('input[id=campo_pesquisa]', 'Clinica Medico');
-        browser.click('button[id=btn_pesquisar]'),
-            function (result) {
-                this.log("Value: ", result.value);
-            }
+        browser
+            .expect.element('button[id=btn_pesquisar]').to.not.be.enabled
         browser.end();
-    }
+    },
+
+    'Testar o botão pesquisar com valor no input': function (browser) {
+        let url = browser.launchUrl + gm;
+        browser
+            .url(url)
+            .waitForElementVisible('body')
+        browser
+            .expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser
+            .clearValue('input[id=campo_pesquisa]')
+            .setValue('input[id=campo_pesquisa]', 'Portal Unimed')
+        browser.expect.element('button[id=btn_pesquisar]').to.be.enabled
+        browser.end();
+    },
+
+    'Testar o botão pesquisar com 2 caracteres': function (browser) {
+        let url = browser.launchUrl + gm;
+        browser
+            .url(url)
+            .waitForElementVisible('body')
+        browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser
+            .clearValue('input[id=campo_pesquisa]')
+            .setValue('input[id=campo_pesquisa]', 'Portal Unimed')
+        browser.expect.element('button[id=btn_pesquisar]').to.be.enabled
+        for (let index = 0; index < 11; index++) {
+            browser.sendKeys('input[id=campo_pesquisa]', browser.Keys.BACK_SPACE)  
+        }
+        browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser.end();
+    },
+
+    'Testar o botão pesquisar apagando valor do input': function (browser) {
+        let url = browser.launchUrl + gm;
+        browser
+            .url(url)
+            .waitForElementVisible('body')
+        browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser
+            .clearValue('input[id=campo_pesquisa]')
+            .setValue('input[id=campo_pesquisa]', 'Portal Unimed')
+        browser.expect.element('button[id=btn_pesquisar]').to.be.enabled
+        for (let index = 0; index < 13; index++) {
+            browser.sendKeys('input[id=campo_pesquisa]', browser.Keys.BACK_SPACE)  
+        }
+        browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser.end();
+    },
 };
