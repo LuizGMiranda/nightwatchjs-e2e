70cef96e72ab806af09006dc5b995908d92b81dc
Merge Desenv <luiz.miranda@portalunimed.com.br>
2019-01-07 11:43:28 -0200

    Ajuste com novas funções do git

--- test/e2e/botaoPesquisar.js
+++ test/e2e/botaoPesquisar.js
@@ -1,23 +1,17 @@
 var config = require('../../nightwatch.conf.js');
 var utils = require('../functions/utils');
-var gm = "/guia-medico";
+var gm = '/guia-medico';
 
 module.exports = {
 
     'Verificar se o botão pesquisar esta ativo': function (browser) {
-        let url = browser.launchUrl + gm;
-        browser
-            .url(url)
-            .waitForElementVisible('body');
+        utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled;
         browser.end();
     },
 
     'Testar o botão pesquisar com valor no input': function (browser) {
-        let url = browser.launchUrl + gm;
-        browser
-            .url(url)
-            .waitForElementVisible('body');
+        utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled;
         utils(browser).inserirValorInput('campo_pesquisa', 'Portal Unimed');
         browser.expect.element('button[id=btn_pesquisar]').to.be.enabled;
@@ -25,10 +19,7 @@ module.exports = {
     },
 
     'Testar o botão pesquisar com 2 caracteres': function (browser) {
-        let url = browser.launchUrl + gm;
-        browser
-            .url(url)
-            .waitForElementVisible('body');
+        utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled;
         utils(browser).inserirValorInput('campo_pesquisa', 'Portal Unimed');
         browser.expect.element('button[id=btn_pesquisar]').to.be.enabled;
@@ -38,10 +29,7 @@ module.exports = {
     },
 
     'Testar o botão pesquisar apagando valor do input': function (browser) {
-        let url = browser.launchUrl + gm;
-        browser
-            .url(url)
-            .waitForElementVisible('body');
+        utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled;
         utils(browser).inserirValorInput('campo_pesquisa', 'Portal Unimed');
         browser.expect.element('button[id=btn_pesquisar]').to.be.enabled;
@@ -51,10 +39,7 @@ module.exports = {
     },
 
     'Testar o botão pesquisar com cpf': function (browser) {
-        let url = browser.launchUrl + gm;
-        browser
-            .url(url)
-            .waitForElementVisible('body');
+        utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled;
         utils(browser).inserirValorInput('campo_pesquisa', 'Portal Unimed');
         utils(browser).inserirValorInput('input_cpf', '85621203836');
@@ -63,10 +48,7 @@ module.exports = {
     },
 
     'Testar o botão pesquisar com cpf incompleto': function (browser) {
-        let url = browser.launchUrl + gm;
-        browser
-            .url(url)
-            .waitForElementVisible('body');
+        utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled;
         utils(browser).inserirValorInput('campo_pesquisa', 'Portal Unimed');
         utils(browser).inserirValorInput('input_cpf', '8562120');
@@ -75,10 +57,7 @@ module.exports = {
     },
 
     'Testar o botão pesquisar apagando valor do cpf e sem valor no campo busca': function (browser) {
-        let url = browser.launchUrl + gm;
-        browser
-            .url(url)
-            .waitForElementVisible('body')
+        utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
         utils(browser).inserirValorInput('input_cpf', '85621203836');
         utils(browser).apagarValorInput('input_cpf', 14);
