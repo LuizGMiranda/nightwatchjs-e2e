c3388eb544bdd13e660f74da92a37309ed69b775
Merge Desenv <luiz.miranda@portalunimed.com.br>
2019-01-07 10:18:15 -0200

    Adicionado novos cenarios de teste

--- test/e2e/botaoPesquisar.js
+++ test/e2e/botaoPesquisar.js
@@ -60,4 +60,58 @@ module.exports = {
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
         browser.end();
     },
+
+    'Testar o botão pesquisar com cpf': function (browser) {
+        let url = browser.launchUrl + gm;
+        browser
+            .url(url)
+            .waitForElementVisible('body')
+        browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser
+            .clearValue('input[id=campo_pesquisa]')
+            .setValue('input[id=campo_pesquisa]', 'Portal Unimed')
+            .clearValue('input[id=input_cpf]')
+            // CPF VALIDO 85621203836 
+            .setValue('input[id=input_cpf]', '85621203836')
+        browser.expect.element('button[id=btn_pesquisar]').to.be.enabled
+        browser.end();
+    },
+
+    'Testar o botão pesquisar com cpf incompleto': function (browser) {
+        let url = browser.launchUrl + gm;
+        browser
+            .url(url)
+            .waitForElementVisible('body')
+        browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser
+            .clearValue('input[id=campo_pesquisa]')
+            .setValue('input[id=campo_pesquisa]', 'Portal Unimed')
+            .clearValue('input[id=input_cpf]')
+            // CPF VALIDO 85621203836 
+            for (let index = 0; index < 13; index++) {
+                let cpf= [8,5,6,2,1,2,0,3,8,3,6]
+                browser.setValue('input[id=input_cpf]', cpf[index])
+            }
+        browser.expect.element('button[id=btn_pesquisar]').to.be.enabled
+        browser.end();
+    },
+
+    'Testar o botão pesquisar com apagando valor do cpf e sem valor no campo busca': function (browser) {
+        let url = browser.launchUrl + gm;
+        browser
+            .url(url)
+            .waitForElementVisible('body')
+        browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
+        browser
+            .clearValue('input[id=campo_pesquisa]')
+            .setValue('input[id=campo_pesquisa]', 'Portal Unimed')
+            .clearValue('input[id=input_cpf]')
+            // CPF VALIDO 85621203836 
+            .setValue('input[id=input_cpf]', '85621203836')
+            for (let index = 0; index < 14; index++) {
+                browser.sendKeys('input[id=input_cpf]', browser.Keys.BACK_SPACE)  
+            }
+        browser.expect.element('button[id=btn_pesquisar]').to.be.enabled
+        browser.end();
+    },
 };
