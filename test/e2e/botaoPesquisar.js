888f72e31c5f16022b0fbe3c075c57c15c7952de
Luiz G Miranda <luiz.g.miranda@gmail.com>
2019-01-08 16:04:56 -0200

    Adicionado teste e ajustado variaveis

--- test/e2e/botaoPesquisar.js
+++ test/e2e/botaoPesquisar.js
@@ -1,9 +1,8 @@
-var config = require('../../nightwatch.conf.js');
-var utils = require('../functions/utils');
-var gm = '/guia-medico';
+const config = require('../../nightwatch.conf.js');
+const utils = require('../functions/utils');
+const gm = '/guia-medico';
 
 module.exports = {
-
     'Verificar se o botão pesquisar esta ativo': function (browser) {
         utils(browser).openUrl(gm)
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled;
@@ -64,4 +63,26 @@ module.exports = {
         browser.expect.element('button[id=btn_pesquisar]').to.not.be.enabled
         browser.end();
     },
+
+    'Testar botão pesquisar com busca avançada': function (browser) {
+        utils(browser).openUrl(gm + '?cdUnimed=25');
+        browser.click('#busca_detalhada');
+        browser.useXpath();
+        browser.verify.cssClassPresent('//*[@id="busca_detalhada"]', 'bold');
+        browser.useCss();
+        browser.moveToElement('#btn_pesquisar', 20, 10);
+        browser.pause(1000);
+        browser.expect.element('button[id=btn_pesquisar]').to.be.enabled;
+        browser.end();
+    },
+
+    'Testar clicar no botão pesquisar e trocar pagina': function (browser){
+        let msg = 'Selecione o estado e a cidade para localizar a Unimed onde você deseja ser atendido:'
+        utils(browser).openUrl(gm);
+        utils(browser).inserirValorInput('campo_pesquisa', 'Portal Unimed');
+        browser.click('#btn_pesquisar');
+        browser.pause(1000);
+        browser.assert.containsText('#texto_selecione_unimed',msg)
+        browser.end();
+    }
 };
